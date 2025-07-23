44a04a2e546b5c7cd5d2cc73bc3e9b2975546a7e
aman1361 <aman.rezaei@gmail.com>
2025-07-22 19:34:52 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ function copyToClipboard(id) {
     x.className = "show";
     setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
 }
-var countDownDate = new Date("Jan 26, 2019 10:00:00").getTime();
+var countDownDate = new Date("Sept 28, 2025 10:00:00").getTime();
 var x = setInterval(function () {
     var now = new Date().getTime();
     var distance = countDownDate - now;
